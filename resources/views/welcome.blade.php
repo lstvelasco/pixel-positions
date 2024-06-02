8ee23549603340ec3edb551be7c1a1263198d88b
Luisito Velasco <lstvelasco@gmail.com>
2024-06-02 20:14:19 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<x-layout>
+    <div class="space-y-10">
+        <section>
+            <x-section-heading> Featured Jobs </x-section-heading>
+    
+            <div class="grid lg:grid-cols-3 gap-8 mt-6">
+                <x-job-card />
+                <x-job-card />
+                <x-job-card />
+            </div>
+        </section>
+    
+        <section>
+            <x-section-heading>Tags</x-section-heading>
+            <div class="mt-6 space-x-1">
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+            </div>
+        </section>
+    
+        <section>
+            <x-section-heading>Recent Jobs</x-section-heading>
+        </section>
+    </div>
+</x-layout>
